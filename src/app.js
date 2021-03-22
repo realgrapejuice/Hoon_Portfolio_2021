455244fb83017387d83939bb9c33d390e9124ba5
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-03-22 17:05:09 +0900

    add hamburger menu

---
+++ src/app.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const burgerMenu = document.querySelector(".burger");
+const burgerBtn = document.querySelector(".burger__btn");
+const timesBtn = document.querySelector(".times__btn");
+
+const leftToRight = (ul) => {
+  ul.style.animation = "leftToRight 500ms linear";
+  ul.style.backgroundColor = "#edc87d";
+};
+
+const burgerClickEventHandler = () => {
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "burger__list");
+  const menuArr = ["Home", "About me", "Skills", "Projects", "Contact"];
+  menuArr.map((element) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    li.setAttribute("class", "burger__list-item");
+    a.setAttribute("href", "#");
+    a.setAttribute("class", "burger__list-link");
+    a.innerText = element;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+  leftToRight(ul);
+  burgerMenu.appendChild(ul);
+};
+
+const toggleClass = () => {
+  burgerBtn.classList.toggle("toggled");
+  timesBtn.classList.toggle("toggled");
+};
+
+const clearUl = () => {
+  const ul = document.querySelector(".burger__list");
+  burgerMenu.removeChild(ul);
+};
+
+burgerBtn.addEventListener("click", () => {
+  burgerClickEventHandler();
+  toggleClass();
+});
+
+timesBtn.addEventListener("click", () => {
+  clearUl();
+  toggleClass();
+});
