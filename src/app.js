b795e3c52347dfe5a9bcd037d588cdcfe1fa569e
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-03-23 18:43:11 +0900

    스크롤이 내려가는 경우 네비게이션 바에서 현재 섹션이 표기될 수 있도록 구현함

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ const burgerClickEventHandler = () => {
     li.setAttribute("class", "burger__list-item");
     a.setAttribute("href", `#${target}`);
     a.setAttribute("class", "burger__list-link");
+    a.setAttribute("data-link", `#${target}`);
     a.innerText = element;
     li.appendChild(a);
     ul.appendChild(li);
@@ -52,3 +53,43 @@ timesBtn.addEventListener("click", () => {
   clearUl();
   toggleClass();
 });
+
+// Relative with scroll alarm
+const sectionIds = ["#home", "#about", "#skills", "#works", "#contact"];
+
+// Id에 부합하는 섹션을 찾아서 배열로 만들어주는 코드
+const sections = sectionIds.map((id) => document.querySelector(id));
+// Nav-bar에서 해당 Id들의 네비게이션 버튼을 찾아주는 코드
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+// Selected Nav Item을 이용해 네비게이션 배열의 첫번째 요소를 선택하는 코드
+let selectedNavItem = navItems[0];
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    let selectedIndex;
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      // 스크롤이 아래로 진행되어 페이지가 올라오는 경우
+      if (entry.boundingClientRect.y < 0) {
+        selectedIndex = index + 1;
+      } else {
+        selectedIndex = index - 1;
+      }
+      selectedNavItem.classList.remove("active");
+      selectedNavItem = navItems[selectedIndex];
+      selectedNavItem.classList.add("active");
+    }
+  });
+};
+
+const observerOption = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observer = new IntersectionObserver(callback, observerOption);
+sections.forEach((section) => observer.observe(section));
